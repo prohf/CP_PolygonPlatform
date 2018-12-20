fac186ec167711764a4f769fedbd6b1a21b9d120
prohf <hfhuangfan@hotmail.com>
2018-12-20 21:53:29 +0800

    今日进度

--- CP_PolygonPlatform/CP_SweepLine.cpp
+++ CP_PolygonPlatform/CP_SweepLine.cpp
@@ -3,7 +3,7 @@
 #include <iterator>
 
 void SweepEvent::setLeftFlag() {
-  if (other_event) {
+  if (other_event != nullptr) {
     auto other_point = other_event->point;
     if (Less(point->m_x, other_point->m_x)) {
       left = true;
@@ -20,35 +20,34 @@ void SweepEvent::setLeftFlag() {
   }
 }
 
-EventQueue initializeQueue(const CP_Polygon & polygon) {
-  EventQueue queue;
+void initializeQueue(const CP_Polygon & polygon, EventQueue & event_queue, PolygonType type) {
   // TODO: 完成初始化
   int loop_first_id = 0;
   int loop_size = 0;
   // 遍历多边形的所有区域
   for (auto region : polygon.m_regionArray) {
     // 遍历区域中的所有环
-    for (auto loop : region.m_loopArray) {
-      // 设置起始id和环的大小
+    for (int l = 0; l < region.m_loopArray.size(); ++l) {
+      auto loop = region.m_loopArray[l];
       loop_first_id = loop.m_pointIDArray[0];
       loop_size = loop.m_pointIDArray.size();
-      // 对环中的每一个点进行处理
+      int d = l == 0 ? 1: -1;
       for (int i = 0; i < loop_size; ++i) {
-        auto & source = polygon.m_pointArray[loop_first_id + i % loop_size];
-        auto & target = polygon.m_pointArray[loop_first_id + (i + 1) % loop_size];
+        auto & source = polygon.m_pointArray[loop_first_id + d * (i % loop_size)];
+        auto & target = polygon.m_pointArray[loop_first_id + d * ((i + 1) % loop_size)];
         // 初始化SweepEvent
-        SweepEvent event_s(source, polygon), event_t(target, polygon);
+        SweepEvent event_s(source, type), event_t(target, type);
         event_s.other_event = std::make_shared<SweepEvent>(event_t);
         event_t.other_event = std::make_shared<SweepEvent>(event_s);
         // 判断哪个是左边的点
         event_s.setLeftFlag();
+        event_t.setLeftFlag();
         // 加入队列中
-        queue.push(event_s);
-        queue.push(event_t);
+        event_queue.push(event_s);
+        event_queue.push(event_t);
       }
     }
   }
-  return queue;
 }
 
 // 第一阶段算法：分割边
@@ -56,10 +55,10 @@ EventQueue initializeQueue(const CP_Polygon & polygon) {
 void subdivision(EventQueue & event_queue, CP_Polygon & result) {
   StatusSet status_set;
   while (!event_queue.empty()) {
-    auto & event = event_queue.top();
+    auto & sweep_event = event_queue.top();
     event_queue.pop();
-    if (event.left) { // left endpoint
-      auto position_it = status_set.insert(event).first;
+    if (sweep_event.left) { // left endpoint
+      auto position_it = status_set.insert(sweep_event).first;
       auto position = *position_it;
       if (position_it != status_set.begin()) {
         auto previous = *std::prev(position_it, 1);
@@ -71,18 +70,20 @@ void subdivision(EventQueue & event_queue, CP_Polygon & result) {
       }
     }
     else { // right endpoint
-      auto position_it = status_set.find(*event.other_event);
-      if (position_it != status_set.begin() && position_it != std::prev(status_set.end())) {
-        auto previous = *std::prev(position_it, 1);
-        auto next = *std::next(position_it, 1);
-        possibleIntersection(previous, next, event_queue, result);
+      auto position_it = status_set.find(*sweep_event.other_event);
+      if (position_it != status_set.end()) { // 点在status_set中
+        if (position_it != status_set.begin() && position_it != std::prev(status_set.end())) {
+          auto previous = *std::prev(position_it, 1);
+          auto next = *std::next(position_it, 1);
+          possibleIntersection(previous, next, event_queue, result);
+        }
+        status_set.erase(position_it);
       }
-      status_set.erase(position_it);
     }
     // if event is in result
     // add event to the result
-    if (&*event.polygon == &result) {
-      result.m_pointArray.push_back(*event.point);
+    if (sweep_event.polygon_type == PolygonType::kPolygonResult) {
+      result.m_pointArray.push_back(*sweep_event.point);
     }
   }
 }
@@ -97,18 +98,20 @@ double toLeftTest(const CP_Point& a, const CP_Point& b, const CP_Point& p) {
 }
 
 // 一线段的端点在另一线段中，处理该情况的事件
-void intersectAtEndpoint(SweepEvent & intersect, SweepEvent& endpoint1, SweepEvent& endpoint2, EventQueue & event_queue, CP_Polygon & result) {
+void intersectAtEndpoint(SweepEvent & intersect, SweepEvent& endpoint1, SweepEvent& endpoint2, EventQueue & event_queue) {
   // 生成两个事件，和线段端点关联，并插入队列中
-  CP_Point point = *intersect.point;
-  SweepEvent intersect1(point, result), intersect2(point, result);
-  intersect1.other_event = std::make_shared<SweepEvent>(endpoint1);
-  endpoint1.other_event = std::make_shared<SweepEvent>(intersect1);
-  intersect2.other_event = std::make_shared<SweepEvent>(endpoint2);
-  endpoint2.other_event = std::make_shared<SweepEvent>(intersect2);
-  intersect1.setLeftFlag();
-  intersect2.setLeftFlag();
-  event_queue.push(intersect1);
-  event_queue.push(intersect2);
+  if (intersect.polygon_type != PolygonType::kPolygonResult) { // 必须不是相交的交点
+    CP_Point point = *intersect.point;
+    SweepEvent intersect1(point, PolygonType::kPolygonResult), intersect2(point, PolygonType::kPolygonResult);
+    intersect1.other_event = std::make_shared<SweepEvent>(endpoint1);
+    endpoint1.other_event = std::make_shared<SweepEvent>(intersect1);
+    intersect2.other_event = std::make_shared<SweepEvent>(endpoint2);
+    endpoint2.other_event = std::make_shared<SweepEvent>(intersect2);
+    intersect1.setLeftFlag();
+    intersect2.setLeftFlag();
+    event_queue.push(intersect1);
+    event_queue.push(intersect2);
+  }
 }
 
 // 计算可能存在的线段相交
@@ -146,25 +149,25 @@ int possibleIntersection(SweepEvent & ab, SweepEvent & uv, EventQueue & event_qu
     CP_Point i(a.m_x + s * p.m_x, a.m_y + s * p.m_y); // 交点为i
     // 修改SweepEvent,将其插入队列中
     // i<->a
-    SweepEvent event_i_a(i, result);
+    SweepEvent event_i_a(i, PolygonType::kPolygonResult);
     event_i_a.other_event = std::make_shared<SweepEvent>(ab);
     ab.other_event = std::make_shared<SweepEvent>(event_i_a);
     event_i_a.setLeftFlag();
     event_queue.push(event_i_a);
     // i<->b
-    SweepEvent event_i_b(i, result);
+    SweepEvent event_i_b(i,PolygonType::kPolygonResult);
     event_i_b.other_event = ba;
     ba->other_event = std::make_shared<SweepEvent>(event_i_b);
     event_i_b.setLeftFlag();
     event_queue.push(event_i_b);
     // i<->u
-    SweepEvent event_i_u(i, result);
+    SweepEvent event_i_u(i, PolygonType::kPolygonResult);
     event_i_u.other_event = std::make_shared<SweepEvent>(uv);
     uv.other_event = std::make_shared<SweepEvent>(event_i_u);
     event_i_u.setLeftFlag();
     event_queue.push(event_i_u);
     // i<->v
-    SweepEvent event_i_v(i, result);
+    SweepEvent event_i_v(i, PolygonType::kPolygonResult);
     event_i_v.other_event = vu;
     vu->other_event = std::make_shared<SweepEvent>(event_i_v);
     event_i_v.setLeftFlag();
@@ -174,42 +177,25 @@ int possibleIntersection(SweepEvent & ab, SweepEvent & uv, EventQueue & event_qu
   // TODO：一条线段的端点在另一条线段上
   // u 在 ab 上
   if (Equal(d_abu, 0.0) && NotEqual(d_abv, 0.0) && NotEqual(d_uva, 0.0) && NotEqual(d_uvb, 0.0)) {
-    intersectAtEndpoint(uv, ab, *ba, event_queue, result);
+    intersectAtEndpoint(uv, ab, *ba, event_queue);
     return 1;
   }
   // v 在 ab 上
   if (NotEqual(d_abu, 0.0) && Equal(d_abv, 0.0) && NotEqual(d_uva, 0, 0) && NotEqual(d_uvb, 0.0)) {
-    intersectAtEndpoint(*vu, ab, *ba, event_queue, result);
+    intersectAtEndpoint(*vu, ab, *ba, event_queue);
     return 1;
   }
   // a 在 uv 上
   if (NotEqual(d_abu, 0.0) && NotEqual(d_abv, 0.0) && Equal(d_uva, 0.0) && NotEqual(d_uvb, 0.0)) {
-    intersectAtEndpoint(ab, uv, *vu, event_queue, result);
+    intersectAtEndpoint(ab, uv, *vu, event_queue);
     return 1;
   }
   // b 在 uv 上
   if (NotEqual(d_abu, 0.0) && NotEqual(d_abv, 0.0) && NotEqual(d_uva, 0.0) && Equal(d_uvb, 0.0)) {
-    intersectAtEndpoint(*ba, uv, *vu, event_queue, result);
+    intersectAtEndpoint(*ba, uv, *vu, event_queue);
     return 1;
   }
   // TODO: 两线段重叠
-  //if (Equal(d_abu, 0.0) && Equal(d_abv, 0.0)) {
-  //  // 从左到右排序
-  //  EventQueue tmp_queue;                                                                        
-  //  tmp_queue.push(ab);
-  //  tmp_queue.push(*ba);
-  //  tmp_queue.push(uv);
-  //  tmp_queue.push(*vu);
-  //  auto left = tmp_queue.top();
-  //  tmp_queue.pop();
-  //  while (!tmp_queue.empty()) {
-  //    auto right = tmp_queue.top();
-  //    CP_Point right_point;
-  //    left.other_event = std::make_shared<SweepEvent>(right);
-  //    right.other_event = std::make_shared<SweepEvent>(left);
-  //    
-  //  }
-  //}
   // 两条线段相交于端点
   // 不相交
   return 0;
--- CP_PolygonPlatform/CP_SweepLine.h
+++ CP_PolygonPlatform/CP_SweepLine.h
@@ -4,47 +4,6 @@
 #include <set>
 // 定义容差
 const double TOLERANCE = 1e-6;
-// 定义向量类型CP_Vector
-typedef CP_Point CP_Vector;
-
-class SweepEvent {
-public: 
-  SweepEvent() = default;
-  SweepEvent(const CP_Point& point, const CP_Polygon& polygon) :
-    point(std::make_shared<CP_Point>(point)),
-    polygon(std::make_shared<CP_Polygon>(polygon)) {}
-  ~SweepEvent() = default;
-  void setLeftFlag();
-
-public:
-  // fields for first stage
-  std::shared_ptr<CP_Point> point;  // point assoiated with this event 
-  bool left;   // is the left endpoint of the edge
-  std::shared_ptr<SweepEvent> other_event;  // other event of the edge
-  std::shared_ptr<CP_Polygon> polygon; // polygon this point belongs to
-
-  // fields for second stage
-  int pos;
-  bool result_in_out;
-  int contour_id;
-  int parent_id; 
-  bool processed;
-  int depth;
-};
-
-struct queue_comparator {
-  inline bool operator() (const SweepEvent& a, const SweepEvent& b) const {
-    return a.point->m_x < b.point->m_x;
-  }
-};
-struct status_comparator {
-  inline bool operator() (const SweepEvent& a, const SweepEvent& b) const {
-    return a.point->m_y < b.point->m_y;
-  }
-};
-typedef std::priority_queue<SweepEvent, std::vector<SweepEvent>, queue_comparator> EventQueue;
-typedef std::set<SweepEvent, status_comparator> StatusSet;
-
 // 浮点数比较运算
 inline bool Equal(double a, double b, double tolerance = TOLERANCE) noexcept { return b - a <= tolerance && a - b <= tolerance; }
 
@@ -57,9 +16,86 @@ inline bool GreatEqual(double a, double b, double tolerance = TOLERANCE) noexcep
 inline bool Less(double a, double b, double tolerance = TOLERANCE) noexcept { return a + tolerance < b; }
 
 inline bool LessEqual(double a, double b, double tolerance = TOLERANCE) noexcept { return a < b + tolerance; }
+// 定义向量类型CP_Vector
+typedef CP_Point CP_Vector;
+enum PolygonType {
+  kPolygonA,
+  kPolygonB,
+  kPolygonResult
+};
+
+class SweepEvent {
+public: 
+  SweepEvent(const CP_Point& point, PolygonType type) :
+    point(std::make_shared<CP_Point>(point)),
+    polygon_type(type) {}
+  ~SweepEvent() = default;
+  void setLeftFlag();
+
+public:
+  // fields for first stage
+  std::shared_ptr<CP_Point> point;  // point assoiated with this event 
+  bool left;   // is the left endpoint of the edge
+  std::shared_ptr<SweepEvent> other_event;  // other event of the edge
+  //std::shared_ptr<CP_Polygon> polygon; // polygon this point belongs to
+  PolygonType polygon_type;
+
+  // fields for second stage
+  int pos;
+  bool result_in_out;
+  int contour_id;
+  int parent_id; 
+  bool processed;
+  int depth;
+};
+
+struct queue_comparator {
+  bool operator() (const SweepEvent& a, const SweepEvent& b) const {
+    //return a.point->m_x > b.point->m_x;
+    if (Great(a.point->m_x , b.point->m_x)) {
+      return true;
+    }
+    else if (Equal(a.point->m_x, b.point->m_x) && Great(a.point->m_y, b.point->m_y)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+};
+
+struct status_comparator { 
+  bool operator() (const SweepEvent& a, const SweepEvent& b) const {
+    if (Great(a.point->m_y, b.point->m_y)) {
+      return true;
+    }
+    else if (Equal(a.point->m_y, b.point->m_y) && Great(a.point->m_x, b.point->m_x)) {
+      return true;
+    }
+    // 两个事件对应的点相同，比较其关联的点。
+    else if (Equal(a.point->m_x, b.point->m_x) && Equal(a.point->m_y, b.point->m_y)) {
+      CP_Point a_o = *a.other_event->point, b_o = *b.other_event->point;
+      if (Great(a_o.m_y, b_o.m_y)) {
+        return true;
+      }
+      else if (Equal(a_o.m_y, b_o.m_y) && Great(a_o.m_x, b_o.m_x)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+  }
+};
+
+typedef std::priority_queue<SweepEvent, std::vector<SweepEvent>, queue_comparator> EventQueue;
+typedef std::set<SweepEvent, status_comparator> StatusSet;
 
 // 初始化SweepEvent优先队列
-extern EventQueue initializeQueue(const CP_Polygon& polygon);  
+extern void initializeQueue(const CP_Polygon& polygon, EventQueue & event_queue, PolygonType type);  
 // First Stage: 分割边
 extern void subdivision(EventQueue& event, CP_Polygon& result);  
 // set information
